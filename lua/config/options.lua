fb9c23d33ba438a7e1b2262e91815ad759f9c5a8
alanmcilwaine <alan.j.mcilwaine@gmail.com>
2024-05-06 12:40:47 +1200

    Added c/c++ lsp

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,3 +5,5 @@ vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
 vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
 vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
 vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
+
+vim.env.CLANG_FORMAT_STYLE = vim.fn.expand("~/.config/nvim/.clang-format")
